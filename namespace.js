d808dc43a6728380c4e00ee45f0e685216ef93dd
phucthien1995 <banh0006@algonquinlive.com>
2018-10-26 17:00:00 -0400

    fix error

--- namespace.js
+++ namespace.js
@@ -1,5 +1,30 @@
 var PHUCTHIEN1995 = {
-    init: function() {
+    init : function() {
         let myBox = document.createElement("div");
+        myBox.className = "box";
+        myBox.textContent = "banh0006";
+        
+        myBox.addEventListener("click", divClick);
+
+        myBox.addEventListener("mouseover", mouseOver);
+        
+        myBox.addEventListener("mouseout", mouseOut);
+        
+        function divClick() {
+            this.style.borderColor = "Chocolate";
+            this.style.backgroundColor = "LightGrey";
+        }
+        
+        function mouseOver(e) {
+            e.target.classList.toggle("highlight");
+        }
+        
+        function mouseOut(e) {
+            e.target.classList.toggle("highlight");
+            e.target.removeAttribute('style');
+        }
+        
+        let boxes = document.querySelector("#boxes");
+        boxes.appendChild(boxes);
     }
-}
+};
